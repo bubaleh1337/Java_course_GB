649a38df99e66cf01488f005a773a58665a7f6cb
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-06-15 17:38:48 +0500

    updated solution for hw2 sem2 (second version: with count)

--- sem2/hw2/task1.java
+++ sem2/hw2/task1.java
@@ -40,38 +40,36 @@ public class task1 {
     StringBuilder sb = new StringBuilder(head);
     String words = "name: , country: , city: , age: ";
     for (int i = 0; i < words.length(); i++) {
-      //words.charAt(i);
-      prompt(words.substring(i), sb);
+      prompt(i, words.substring(i).toString(), sb);
     }
   }
 
-  public static StringBuilder prompt(String msg, StringBuilder sb) {
+  public static StringBuilder prompt(int count, String words, StringBuilder sb) {
     Scanner scanner = new Scanner(System.in);
-    addParametr(msg, sb);
+    addParametr(count, words, sb);
     String word = scanner.nextLine();
     sb.append(word);
     scanner.close();
     return sb;
   }
 
-  public static void addParametr(String word, StringBuilder sb) {
-    //String str_words = words.toString();
-    switch (word) {
-      case "name":
+  public static void addParametr(int count, String words, StringBuilder sb) {
+    switch (count) {
+      case 0:
         System.out.println("Enter a name: ");
-        sb.append(sb.charAt(0));
+        sb.append(words.charAt(0));
         break;
-      case "country":
+      case 1:
         System.out.println("Enter a country: ");
-        sb.append(sb.charAt(1));
+        sb.append(words.charAt(1));
         break;
-      case "city":
+      case 2:
         System.out.println("Enter a city: ");
-        sb.append(sb.charAt(2));
+        sb.append(words.charAt(2));
         break;
-      case "age":
+      case 3:
         System.out.println("Enter a age: ");
-        sb.append(sb.charAt(3));
+        sb.append(words.charAt(3));
         break;
       default:
         System.out.println("Something wrong. Try again.");
